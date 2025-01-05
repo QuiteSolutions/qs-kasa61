d6f9ff12cb671d9b85fa6cae095010ce91a1e6dc
AlexHomePC <gotlib14@gmail.com>
2025-01-05 16:53:25 +0200

    test 3

--- pos_display_general_note/static/src/js/pos_general_note.js
+++ pos_display_general_note/static/src/js/pos_general_note.js
@@ -2,14 +2,48 @@
 import { registry } from '@web/core/registry';
 import { TicketScreen } from '@point_of_sale/screens/ticket_screen/ticket_screen';
 
-export class TicketScreenWithGeneralNote extends TicketScreen {
-    getFilteredOrderList() {
-        const orders = super.getFilteredOrderList();
-        return orders.map(order => {
-            order.general_note = order.general_note || 'N/A';
-            return order;
-        });
-    }
-}
+// export class TicketScreenWithGeneralNote extends TicketScreen {
+//     getFilteredOrderList() {
+//         const orders = super.getFilteredOrderList();
+//         return orders.map(order => {
+//             order.general_note = order.general_note || 'N/A';
+//             return order;
+//         });
+//     }
+// }
 
-registry.category('pos_screens').add('TicketScreen', TicketScreenWithGeneralNote, { replace: true });
+// registry.category('pos_screens').add('TicketScreen', TicketScreenWithGeneralNote, { replace: true });
+odoo.define('pos_customizations.models', function(require) {
+    'use strict';
+
+    var {Order, OrderLine} = require('point_of_sale.models');
+    const Registries = require('point_of_sale.Registries');
+
+    const PosSaleOrder = (Order) => class PosSaleOrder extends Order {
+        constructor() {
+            super(...arguments);
+            this.general_note = this.general_note || "NUN";
+        }
+
+        export_as_JSON() {
+            const json = super.export_as_JSON(...arguments);
+            json.general_note = this.general_note;
+            return json;
+        }
+
+        init_from_JSON(json) {
+            super.init_from_JSON(...arguments);
+            this.general_note = json.general_note;
+        }
+
+        getFilteredOrderList() {
+            const orders = super.getFilteredOrderList();
+            return orders.map(order => {
+                order.general_note = order.general_note || 'N/A';
+                return order;
+            });
+        }
+    };
+
+    Registries.Model.extend(Order, PosSaleOrder);
+});
