95e8684f4fab988123a88616ed25d205eea99371
Spax04 <gotlib14@gmail.com>
2025-01-05 10:39:10 +0200

    [update] display note

--- pos_display_general_note/__manifest__.py
+++ pos_display_general_note/__manifest__.py
@@ -1,13 +1,17 @@
 {
-    'name': 'POS Order General Note Column',
+    'name': 'POS General Note Column',
     'version': '1.0',
-    'summary': 'Adds a General Note column to POS Orders list view.',
+    'summary': 'Add a General Note column to the POS orders list.',
     'category': 'Point of Sale',
     'author': 'Alexander Gotlib',
-    'depends': ['point_of_sale'],  
-    'data': [
-        'views/pos_order_view.xml',  
-    ],
+    'depends': ['point_of_sale'],
+    'data': [],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_display_general_note/static/src/js/pos_general_note.js',
+            'pos_display_general_note/static/src/xml/pos_templates.xml',
+        ],
+    },
     'installable': True,
     'application': False,
 }
---
+++ pos_display_general_note/static/src/js/pos_general_note.js
@@ -0,0 +1,23 @@
+odoo.define('pos_display_general_note.OrderExtension', function (require) {
+  'use strict';
+
+  const { Order } = require('point_of_sale.models');
+  const models = require('point_of_sale.models');
+
+  // Extend the order model to include the general_note field
+  models.Order = models.Order.extend({
+      initialize(attributes, options) {
+          this.general_note = this.general_note || '';
+          return this._super(attributes, options);
+      },
+      export_as_JSON() {
+          const json = this._super();
+          json.general_note = this.general_note;
+          return json;
+      },
+      init_from_JSON(json) {
+          this._super(json);
+          this.general_note = json.general_note;
+      },
+  });
+});
