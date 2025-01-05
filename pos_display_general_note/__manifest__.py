016e0e0007fe127d54a54e52f947ef261ed516d6
Spax04 <gotlib14@gmail.com>
2025-01-05 10:15:19 +0200

    [add] print and gnote

---
+++ pos_display_general_note/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'POS Order General Note Column',
+    'version': '1.0',
+    'summary': 'Adds a General Note column to POS Orders list view.',
+    'category': 'Point of Sale',
+    'author': 'Alexander Gotlib',
+    'depends': ['point_of_sale'],  
+    'data': [
+        'views/pos_order_view.xml',  
+    ],
+    'installable': True,
+    'application': False,
+}
