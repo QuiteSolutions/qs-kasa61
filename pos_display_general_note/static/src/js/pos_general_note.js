2b5092e20d220e38ac1bcef62fc2da01b2818c57
Spax04 <gotlib14@gmail.com>
2025-01-05 11:09:28 +0200

    xif

--- pos_display_general_note/static/src/js/pos_general_note.js
+++ pos_display_general_note/static/src/js/pos_general_note.js
@@ -3,21 +3,29 @@ odoo.define('pos_display_general_note.OrderExtension', function (require) {
 
   const { Order } = require('point_of_sale.models');
   const models = require('point_of_sale.models');
-
+  console.log('POS General Note JS Loaded');
   // Extend the order model to include the general_note field
   models.Order = models.Order.extend({
-      initialize(attributes, options) {
-          this.general_note = this.general_note || '';
-          return this._super(attributes, options);
-      },
-      export_as_JSON() {
-          const json = this._super();
-          json.general_note = this.general_note;
-          return json;
-      },
-      init_from_JSON(json) {
-          this._super(json);
-          this.general_note = json.general_note;
-      },
-  });
+    export_as_JSON() {
+        const json = this._super();
+        console.log('Exporting General Note:', this.general_note);
+        json.general_note = this.general_note;
+        return json;
+    },
+});
+  // models.Order = models.Order.extend({
+  //     initialize(attributes, options) {
+  //         this.general_note = this.general_note || '';
+  //         return this._super(attributes, options);
+  //     },
+  //     export_as_JSON() {
+  //         const json = this._super();
+  //         json.general_note = this.general_note;
+  //         return json;
+  //     },
+  //     init_from_JSON(json) {
+  //         this._super(json);
+  //         this.general_note = json.general_note;
+  //     },
+  // });
 });
