22ee23752e8b9fbb63abcda353b7309915ff023a
AlexHomePC <gotlib14@gmail.com>
2025-01-05 20:13:33 +0200

    test 5

--- pos_display_general_note/static/src/js/pos_general_note.js
+++ pos_display_general_note/static/src/js/pos_general_note.js
@@ -2,17 +2,14 @@
 import { registry } from '@web/core/registry';
 import { TicketScreen } from '@point_of_sale/screens/ticket_screen/ticket_screen';
 
-export class TicketScreenWithGeneralNote extends TicketScreen {
-    getFilteredOrderList() {
-        const orders = super.getFilteredOrderList();
-        return orders.map(order => {
-            order.general_note = order.general_note || 'N/A';
-            return order;
-        });
-    }
-}
-
- registry.category('pos_screens').add('TicketScreen', TicketScreenWithGeneralNote, { replace: true });
+patch(TicketScreen.prototype, {
+    setup() {
+        super.setup(...arguments);
+        // this.totalPayment = this.currentOrder.getTotalDue();
+        // this.tipValue = 0;
+    },
+});
+ //registry.category('pos_screens').add('TicketScreen', TicketScreenWithGeneralNote, { replace: true });
 // odoo.define('pos_customizations.models', function(require) {
 //     'use strict';
 
