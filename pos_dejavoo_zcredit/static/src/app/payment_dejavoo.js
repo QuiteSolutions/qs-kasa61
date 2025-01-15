1a21bc179ebbd8ea7a0deccc37f8b471134713e2
AlexHomePC <gotlib14@gmail.com>
2025-01-15 18:35:52 +0200

    [upd] updated dejavoo refund module

--- pos_dejavoo_zcredit/static/src/app/payment_dejavoo.js
+++ pos_dejavoo_zcredit/static/src/app/payment_dejavoo.js
@@ -7,7 +7,6 @@ export class PaymentDejavoo extends PaymentInterface {
     async create_payment_intent() {
         const order = this.pos.get_order();
         const line = order.get_selected_paymentline();
-        // Build informations for creating a payment intend on Dejavoo.
         const infos = {
             TransactionSum: line.amount,
             additional_info: {
@@ -15,7 +14,6 @@ export class PaymentDejavoo extends PaymentInterface {
                 print_on_terminal: true,
             },
         };
-        // dj_payment_intent_create will call the Z-Credit api gateway to interact with Dejavoo terminal
         return await this.env.services.orm.silent.call(
             "pos.payment.method",
             "dj_payment_intent_create",
@@ -23,6 +21,30 @@ export class PaymentDejavoo extends PaymentInterface {
         );
     }
 
+    async create_refund_intent(transactionId) {
+        const order = this.pos.get_order();
+    
+        const line = order.get_selected_paymentline();
+        if (!line) {
+            throw new Error("No payment line is selected.");
+        }
+    
+        const infos = {
+            TransactionSum: line.amount * -1,
+            TransactionIdToCancelOrRefund: transactionId,
+            additional_info: {
+                external_reference: `${this.pos.config.current_session_id.id}_${line.payment_method_id.id}_${order.uuid}`,
+                print_on_terminal: true,
+            },
+        };
+    
+        return await this.env.services.orm.silent.call(
+            "pos.payment.method",
+            "dj_payment_refund_create",
+            [[line.payment_method_id.id], infos]
+        );
+    }
+    
     setup() {
         super.setup(...arguments);
         this.webhook_resolver = null;
@@ -35,8 +57,33 @@ export class PaymentDejavoo extends PaymentInterface {
 
 
         if (line.amount < 0) {
-            this._show_error(_t("Cannot process transactions with negative amount."));
-            return Promise.resolve();
+            try{
+                line.set_payment_status("waitingCapture");
+
+                
+                let transaction_id = prompt('הכנס מספר עסקה');
+                let bar = confirm(`מספר עסקה : ${transaction_id} \n סכום לזיכוי: ${line.amount} \n האם אתה בטוח שברצונך לבצע את הזיכוי?`);
+
+                if (!bar) {
+                    this._showMsg(_t("Payment has been canceled by the user"), "info");
+                    line.set_payment_status("rejected");
+                    return false;
+                }
+
+                const data = await this.create_refund_intent(transaction_id);
+                if (data.HasError) {
+                    this._showMsg(data.ReturnMessage, "error");
+                    line.set_payment_status("rejected");
+                    return false;
+                }
+                line.set_payment_status("done");
+                this._showMsg(_t("Payment has been processed successfully"), "info");
+                return true;
+            }catch (error) {
+                this._showMsg(_t("An error occurred while processing the payment: ") + error.message, "error");
+                line.set_payment_status("error");
+                return false;
+            }
         }
 
         try {
@@ -50,12 +97,15 @@ export class PaymentDejavoo extends PaymentInterface {
                 return false;
             }
 
+
+        
             const regex = /<l>(.*?)<\/l><r>(.*?)<\/r>/g;
             let match;
             let formattedString = '';
-
+            
+            formattedString += `${"מספר עסקה"}: ${data?.ReferenceNumber}\n`;
             while ((match = regex.exec(data?.ClientRecieptPP)) !== null) {
-                const key = match[1];
+                const key = match[1]; 
                 const value = match[2]; 
                 formattedString += `${key}: ${value}\n`; 
             }
@@ -74,26 +124,6 @@ export class PaymentDejavoo extends PaymentInterface {
             line.set_payment_status("error");
             return false;
         }
-    }
-
-    parseClientRecieptPP(receiptString) {
-        // Create a DOM parser
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(receiptString, 'text/xml');
-    
-        // Extract all <w> tags which contain <l> and <r>
-        const keyValuePairs = {};
-        const entries = doc.getElementsByTagName('w');
-    
-        for (let entry of entries) {
-            const key = entry.getElementsByTagName('l')[0]?.textContent?.trim();
-            const value = entry.getElementsByTagName('r')[0]?.textContent?.trim();
-            if (key && value) {
-                keyValuePairs[key] = value;
-            }
-        }
-    
-        return keyValuePairs;
     }
 
     async send_payment_cancel(order, cid) {
@@ -120,5 +150,6 @@ export class PaymentDejavoo extends PaymentInterface {
             body: msg,
         });
     }
+
 }
 
--- pos_dejavoo_zcredit/static/src/overrides/models/models.js
+++ pos_dejavoo_zcredit/static/src/overrides/models/models.js
@@ -3,3 +3,4 @@ import { register_payment_method } from "@point_of_sale/app/store/pos_store";
 import { PaymentDejavoo } from "../../app/payment_dejavoo";
 //!
 register_payment_method("dejavoo", PaymentDejavoo);
+
